0be636394beeb5bb3d41aad059038d1b5d93a4a1
Yesbany Ramirez <yramirezalcedo@hotmail.com>
2024-05-12 22:08:10 -0500

    add person global status

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,50 @@
-import './App.css'
-import {useStore} from './globalState/StateGlobal'
+import { useState } from "react";
+import "./App.css";
+import { usePersonStore, useStore } from "./globalState/StateGlobal";
 
 function App() {
-  const count= useStore(state => state.count)
-  const increment= useStore(state => state.increment)
-  const decrement= useStore(state => state.decrement)
+  const count = useStore((state) => state.count);
+  const status = useStore((state) => state.status);
+  const increment = useStore((state) => state.increment);
+  const decrement = useStore((state) => state.decrement);
+  const handleStatusChange = useStore((state) => state.handleStatusChange);
+
+  const { name, lastName, updateName, updateLastName } = usePersonStore();
+
+  const [statusChange, setStatusChange] = useState<string>("");
 
   return (
     <>
-      <h1>{count}</h1>
-      <button onClick={increment}>Incrementar</button>
-      <button onClick={decrement}>Decrementar</button>
+      <div>
+        <h1>{status}</h1>
+        <input
+          onChange={(e) => {
+            setStatusChange(e.target.value);
+          }}
+        />
+        <button onClick={() => handleStatusChange(statusChange)}>
+          Status Change
+        </button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button onClick={increment}>Incrementar</button>
+        <button onClick={decrement}>Decrementar</button>
+      </div>
+      <div>
+        <div style={{border: "1px solid red"}}>
+          <input type="text" onChange={(e) => updateName(e.target.value)} />
+          <h3>Name</h3>
+          <label htmlFor="">{name}</label>
+        </div><br />
+        <div style={{border: "1px solid blue"}}>
+          <input type="text" onChange={(e) => updateLastName(e.target.value)} />
+          <h3>LastName</h3>
+          <label htmlFor="">{lastName}</label>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/globalState/StateGlobal.ts
+++ src/globalState/StateGlobal.ts
@@ -3,20 +3,45 @@ import { persist } from "zustand/middleware";
 
 interface IStore {
   count: number;
+  status: string;
   increment: () => void;
   decrement: () => void;
+  handleStatusChange: (estado: string) => void;
 }
 
 const useStore = create<IStore>()(
   persist(
     (set) => ({
       count: 0,
-      increment: () =>
-        set((state) => ({ count: state.count + 2 })),
-      decrement: () => set((state) => ({count: state.count - 1}))
+      status: "",
+      increment: () => set((state) => ({ count: state.count + 2 })),
+      decrement: () => set((state) => ({count: state.count - 1})),
+      handleStatusChange: (estado) => set(() => ({ status: estado}))
     }),
     { name: "count-storage" }
   )
 );
 
-export { useStore };
+type State = {
+  name: string;
+  lastName: string;
+}
+
+type Action = {
+  updateName: (dataName: string) => void;
+  updateLastName: (dataLastName: string) => void;
+}
+
+const usePersonStore = create<State & Action>()(
+  persist(
+    (set) => ({
+      name: "",
+      lastName: "",
+      updateName: (dataName) => set((state) => ({...state, name: dataName})),
+      updateLastName: (dataLastName) => set(() => ({lastName: dataLastName})),
+    }),
+    {name: "person"}
+  )
+)
+
+export { useStore, usePersonStore };
